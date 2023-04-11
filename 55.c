04946bf9bef0e14bd1f991d586a0a4edc363593a
신동천 <q75020021@icloud.com>
2023-04-11 07:01:42 +0000

    dadad

---
+++ 55.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	
+int a = 3;
+a = a + 6;
+if(a > 5) {
+	printf("%d\n",a);
+	printf("조건이 참일때, 실행됩니다. \n");
+} else {
+	printf("%d\n",a);
+	printf("조건이 거짓일때, 실행됩니다. \n");
+}
+ //printf("%d\n", a - 6);
+}
